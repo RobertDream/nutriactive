554945bb09f44bf5e9a6439ac483971c366b8d6e
daniel_hutanu <danielhutanu22@gmail.com>
2022-02-10 17:17:17 +0200

    daniel code

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const header = document.querySelector('header');
+
+const anchor = document.querySelector('.menu-anchor')
+
+function sticky(header, anchor){
+    header.classList.toggle("sticky", window.scrollY>0);
+    anchor.classList.toggle("sticky", window.scrollY>0);
+
+}
+
+window.addEventListener("scroll", function(){
+sticky(header, anchor);
+if(scrollY == 0){
+    paddingHeader.style.display = "none";
+}
+});
