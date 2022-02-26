3d250d3d84fb1689d09986c8c0116db25a075af9
RobertDream <marius.strugariu17@yahoo.com>
2022-02-26 19:32:22 +0200

    hats

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,11 @@ const anchor = document.querySelector('.menu-anchor')
 function sticky(header, anchor){
     header.classList.toggle("sticky", window.scrollY>0);
     anchor.classList.toggle("sticky", window.scrollY>0);
-
 }
-
 window.addEventListener("scroll", function(){
 sticky(header, anchor);
-if(scrollY == 0){
-    paddingHeader.style.display = "none";
-}
 });
+
+
+ 
+      
