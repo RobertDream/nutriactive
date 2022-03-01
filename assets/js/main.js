c8d9f6de91391d368d87a0f07c08401a4ae7c4ea
RobertDream <marius.strugariu17@yahoo.com>
2022-03-01 09:20:37 +0200

    hats

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 const header = document.querySelector('header');
-
-const anchor = document.querySelector('.menu-anchor')
-
+const anchor = document.querySelector('.menu-anchor');
 function sticky(header, anchor){
     header.classList.toggle("sticky", window.scrollY>0);
     anchor.classList.toggle("sticky", window.scrollY>0);
