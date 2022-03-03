5d7e073bfbec8df5e7295a62ae4ba6b278224d04
daniel_hutanu <danielhutanu22@gmail.com>
2022-03-03 16:19:54 +0200

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,29 @@
-const header = document.querySelector('header');
+const headerNav = document.querySelector('header');
 const anchor = document.querySelector('.menu-anchor');
-function sticky(header, anchor){
-    header.classList.toggle("sticky", window.scrollY>0);
+function sticky(headerNav, anchor){
+    headerNav.classList.toggle("sticky", window.scrollY>0);
     anchor.classList.toggle("sticky", window.scrollY>0);
 }
 window.addEventListener("scroll", function(){
-sticky(header, anchor);
+sticky(headerNav, anchor);
 });
    
+
+
+function openMenu(p){
+    p.style = "display: flex";
+}
+function closeMenu(p){
+    p.style = "display: none";
+}
+
+const btnMenu = document.getElementById('menu-mobile-btn-container');
+const menu = document.querySelector('.menu-mobile-container');
+const closeBtn = document.getElementById('close-menu');
+
+btnMenu.addEventListener("click", function(){
+    openMenu(menu);
+});
+closeBtn.addEventListener("click", function(){
+    closeMenu(menu);
+})
