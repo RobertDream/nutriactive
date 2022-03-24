1eb6c924e3e883978270565c378d4af5ec2b2de5
RobertDream <marius.strugariu17@yahoo.com>
2022-03-24 19:39:04 +0200

    hatz

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ sticky(headerNav, anchor);
 });
    
 
-
+//mobil
 function openMenu(p){
     p.style = "display: flex";
 }
@@ -27,3 +27,35 @@ btnMenu.addEventListener("click", function(){
 closeBtn.addEventListener("click", function(){
     closeMenu(menu);
 })
+
+
+//Galerie
+var elements = document.getElementsByClassName("column");
+var i;
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%"; 
+    elements[i].style.flex = "100%";
+  }
+}
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%"; 
+    elements[i].style.flex = "50%";
+  }
+}
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  
+    elements[i].style.flex = "25%";
+  }
+}
+var header = document.getElementById("myGallery");
+var btns = header.getElementsByClassName("btn-gl");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
